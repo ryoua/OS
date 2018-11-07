c423bc43909509e547b3554b87684b8e3affb6fa
徐炳华 <31928820+ryoua@users.noreply.github.com>
2018-11-07 12:39:01 +0800

    update

--- Project7/CodeWriter.py
+++ Project7/CodeWriter.py
@@ -1,9 +1,17 @@
-def setFileName():
-    pass
+import sys, os
+import Project7.Parser
+CODEFLAG1 = 0
+CODEFLAG2 = 0
 
 
-def writeArithmetic():
-    pass
+def setFileName(filename):
+    filetuple = os.path.splitext(filename)
+    wfile = open(filetuple[0] + '.asm', 'w')
+    return wfile
+
+
+def writeArithmetic(wfile, command):
+
 
 
 def WritePushPop():
--- Project7/Parser.py
+++ Project7/Parser.py
@@ -1,19 +1,37 @@
-def hasMoreCommands():
-    pass
+Arith = ('add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not')
 
 
-def advance():
-    pass
+def hasMoreCommands(line):
+    if not line:
+        return 0
+    else:
+        return 1
+
+
+def advance(rfile):
+    line = rfile.readline()
+    return line
 
 
 def commandType(line):
-    pass
+    if line.find('push') >= 0:
+        return 'C_PUSH'
+    elif line.find('pop') >= 0:
+        return 'C_POP'
+    elif line.strip() in Arith:
+        return 'C_ARITHMATIC'
 
 
-def arg1():
-    pass
+def arg1(line):
+    if commandType(line) is 'C_ARITHMATIC':
+        return line.strip()
+    else:
+        spline = line.split(' ')
+        return spline[1]
 
 
-def Arg2():
-    pass
+def arg2(line):
+    if commandType(line) in ('C_POP', 'C_PUSH', 'C_FUNCTION', 'C_CALL'):
+        spline = line.split(' ')
+        return spline[2]
 
